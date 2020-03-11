2d680a34123ef1dd33731d1520f6f95ec66d447c
happy <whitehatzk520@gmail.com>
2020-03-11 23:42:17 +0800

    :rocket:eureka服务发现

--- cloud-provider-payment-8001/src/main/java/com/elliot/cloud/controller/PaymentController.java
+++ cloud-provider-payment-8001/src/main/java/com/elliot/cloud/controller/PaymentController.java
@@ -1,15 +1,18 @@
 package com.elliot.cloud.controller;
 
-import com.elliot.cloud.service.PaymentService;
 import com.elliot.cloud.entity.CommonResult;
 import com.elliot.cloud.entity.Payment;
+import com.elliot.cloud.service.PaymentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/payment")
@@ -19,6 +22,9 @@ public class PaymentController {
 
   @Resource
   private PaymentService paymentService;
+
+  @Resource
+  private DiscoveryClient discoveryClient;
 
   @Value("${server.port}")
   private String serverPort;
@@ -32,6 +38,7 @@ public class PaymentController {
     } else {
       return new CommonResult<>(500, "fail", payment);
     }
+    
   }
 
   @PostMapping("/add")
@@ -43,6 +50,20 @@ public class PaymentController {
     } else {
       return new CommonResult(500, "add fail", null);
     }
+  }
+
+  @GetMapping("/discovery")
+  @ApiOperation("服务发现")
+  public Object getDiscoveryClient() {
+    List<String> services = discoveryClient.getServices();
+    for (String service : services) {
+      log.info("**service: " + service);
+    }
+    List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+    for (ServiceInstance instance : instances) {
+      log.info("serviceId: " + instance.getServiceId() + " host: " + instance.getHost() + " URL: " + instance.getUri() + instance.getPort());
+    }
+    return this.discoveryClient;
   }
 
 }
