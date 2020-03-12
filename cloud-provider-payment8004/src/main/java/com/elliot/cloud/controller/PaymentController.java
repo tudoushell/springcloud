57e6f4e90414b4c3e2d43f8c55483285bb53751b
happy <whitehatzk520@gmail.com>
2020-03-12 23:36:57 +0800

    :rocket: zookeeper 服务提供者搭建

---
+++ cloud-provider-payment8004/src/main/java/com/elliot/cloud/controller/PaymentController.java
@@ -0,0 +1,19 @@
+package com.elliot.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+
+  @Value("${server.port}")
+  private String port;
+
+  @GetMapping("/zk")
+  public String  paymentZK() {
+    return "springcloud with zookeeper: " + port;
+  }
+}
