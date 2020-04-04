e4bc32e007faf85199782daf4a01f56fcdd5fe10
happy <whitehatzk520@gmail.com>
2020-04-04 15:41:09 +0800

    :rocket: Hystrix dashboard配置

--- cloud-provider-hystrix-payment8001/src/main/java/com/elliot/cloud/HystrixPaymentMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/elliot/cloud/HystrixPaymentMain8001.java
@@ -1,9 +1,12 @@
 package com.elliot.cloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,21 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 public class HystrixPaymentMain8001 {
   public static void main(String[] args) {
     SpringApplication.run(HystrixPaymentMain8001.class, args);
+  }
+
+  /**
+   * 此配置是为了服务监控而配置的，与服务容错本身无关
+   * servletRegistration因为springboot的默认路径不是/hystrix.stream
+   * 只需配置下servlet
+   * @return
+   */
+  @Bean
+  public ServletRegistrationBean getServlet() {
+    HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+    ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+    registrationBean.setLoadOnStartup(1);
+    registrationBean.addUrlMappings("/hystrix.stream");
+    registrationBean.setName("HystrixMetricsStreamServlet");
+    return registrationBean;
   }
 }
