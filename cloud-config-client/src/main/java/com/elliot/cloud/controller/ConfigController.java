b00052ebe8b38db89da02cd6c9ed1376298bee7d
happy <whitehatzk520@gmail.com>
2020-04-06 14:53:56 +0800

    :sparkles: springcloud  config client 文件配置手动刷新

--- cloud-config-client/src/main/java/com/elliot/cloud/controller/ConfigController.java
+++ cloud-config-client/src/main/java/com/elliot/cloud/controller/ConfigController.java
@@ -1,10 +1,18 @@
 package com.elliot.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 想要从配置中心server获取最新的yml，需要3个步骤
+ * 1. 修改bootstrap.yml添加暴露监控端点
+ * 2. 在控制层添加@RefreshScope注解
+ * 3. 用post调用client接口，/actuator/refresh
+ */
 @RestController
+@RefreshScope
 public class ConfigController {
 
   @Value("${config.info}")
