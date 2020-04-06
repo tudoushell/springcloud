fb49b4c1519be9f6c7c80698a36047437ef17710
happy <whitehatzk520@gmail.com>
2020-04-06 14:20:51 +0800

    :sparkles: springcloud  config client 文件配置

---
+++ cloud-config-client/src/main/java/com/elliot/cloud/controller/ConfigController.java
@@ -0,0 +1,17 @@
+package com.elliot.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+  @Value("${config.info}")
+  private String config;
+
+  @GetMapping("/getConfig")
+  public String getConfig () {
+    return config;
+  }
+}
